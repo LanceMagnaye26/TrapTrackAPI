bdd06b69b8ecdc2c90ae10b2a1de02c849d945ee
LanceM26 <lancemagnaye@gmail.com>
2019-04-10 10:26:06 -0700

    removed useless tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
-var request = require('supertest')
-var app = require('../app')
+// var request = require('supertest')
+// var app = require('../app')
 
-describe('Index', function () {
-  it('is the root path', function (done) {
-    request(app).get('/')
-      .expect(200, done)
-  })
-})
+// describe('Index', function () {
+//   it('is the root path', function (done) {
+//     request(app).get('/')
+//       .expect(200, done)
+//   })
+// })
